db38293accdd93ac0ef85916aaff5abd36ce3ea4
Basti <Basti@Basti-PC>
2012-03-31 18:25:00 +0200

    Checking for Liquids and doors (not yet trapdoors) is working!

--- FluidPhys/src/de/nagetier/FluidPhys/DoorPhys.java
+++ FluidPhys/src/de/nagetier/FluidPhys/DoorPhys.java
@@ -32,39 +32,78 @@ public class DoorPhys implements Listener
 		if ((action == Action.RIGHT_CLICK_BLOCK) || (action == Action.LEFT_CLICK_BLOCK))
 		{
 			//Door Block?
-			if((clicked.getType() == Material.IRON_DOOR) || (clicked.getType() == Material.WOODEN_DOOR) || (clicked.getType() == Material.TRAP_DOOR))
+			if(isDoor(clicked))
 			{
+				//Send Material to waterdoor function, no need to change over and over....
+				
+				Material btype = clicked.getType();
+				Door door = (Door) btype.getNewData((clicked.getData()));
+				
 				log.info("Door!?");
-				waterThroughDoor(clicked);
+				waterThroughDoor(door, clicked);
 			}
-			else{	}
+			else
+			{
+				log.info("no door!");
+			}
 		}
 		else{	}
 	}
 	
-	private void waterThroughDoor(Block doorblock)
+	
+	
+	
+	@SuppressWarnings("unused")
+	private void waterThroughDoor(Door door, Block doorblock)
 	{
-		if(doorblock.getState() instanceof Door)
+		log.info("Door!");
+		
+		//Blocks touching the door (Only selecting the upper ones, need to get the lower ones!
+		BlockFace facing = door.getFacing();		
+		Block block_front = doorblock.getRelative(facing);
+		Block block_back = doorblock.getRelative(facing.getOppositeFace());
+		
+		//Get Location of Blocks
+		World w = doorblock.getWorld();
+		Location loc_front = block_front.getLocation();
+		Location loc_back = block_back.getLocation();
+		
+		/*		Check if water on one side of the door
+		 * 		Works, but only checks top block of door, need to check lower block either
+		 * 		Defining lower blocks 
+		 */	
+		
+		Location loc_back_lower = loc_back.add(0, -1, 0);
+		Location loc_front_lower = loc_front.add(0, -1, 0);
+		
+		
+		/*		Blocks at defines positions,
+		 * 		good for checking water conditions. 
+		 */
+		Block b_back = loc_back.getBlock();
+		Block b_back_lower = loc_back_lower.getBlock();
+		Block b_front = loc_front.getBlock();
+		Block b_front_lower = loc_front_lower.getBlock();
+
+		if(b_back.isLiquid() || b_back_lower.isLiquid() || b_front.isLiquid() || b_front_lower.isLiquid())
 		{
-			log.info("Door!");
-			Door door = (Door)doorblock.getState();
-			door.setOpen(false);			
-			BlockFace facing = door.getFacing();
-			//Wheres the block?
-			Location loc = doorblock.getLocation();
-			World w = loc.getWorld();
-			
-			//Blocks touching the door
-			Block block_front = doorblock.getRelative(facing);
-			Block block_back = doorblock.getRelative(facing.getOppositeFace());
-			
-			//Check if water on one side of the door
-			
+			log.info("Liquid!");
+		}
+		
+	}
+	
+	private boolean isDoor(Block clicked_block)
+	{
+		Material btype = clicked_block.getType();
+		
+		if((btype == Material.WOODEN_DOOR) || (btype == Material.IRON_DOOR))
+		{
+			return true;
 		}
 		else
 		{
-			log.info("null");
+			return false;
 		}
-						
 	}
+	
 }
